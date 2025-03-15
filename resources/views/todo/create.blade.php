ddbdc76169149396b550244d6d909c0180ec051b
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-03-15 13:33:22 +0700

    env, mvc, route, relationship

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
                     {{ __('Create Todo Page') }}
                 </div>
             </div>

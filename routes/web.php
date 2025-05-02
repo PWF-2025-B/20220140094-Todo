aa23709a52a38598dd23b08b82356dcb615ae860
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-05-02 18:54:59 +0700

    update, delete data

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Models\Todo;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,11 +22,22 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+
     Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    // Route::patch('todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
 
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 
     Route::resource('todo', TodoController::class)->except(['show']);
+
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+
+    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
+    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
 });
 
 require __DIR__ . '/auth.php';
---
+++ tugas6.md
@@ -0,0 +1,25 @@
+# Tugas 6
+
+## 1. Todo Completed
+![Alt text](screenshot/tugas6/Screeshottodocomplete.png)
+
+## 2. Todo Uncompleted
+![Alt text](screenshot/tugas6/Screenshottodouncomplete.png)
+
+## 3. Todo Edited
+![Alt text](screenshot/tugas6/Screenshotedited.png)
+
+## 4. Todo Deleted
+![Alt text](screenshot/tugas6/Screenshottododelete.png)
+
+## 5. Todo Delete All Completed Todo
+![Alt text](screenshot/tugas6/Screenshotdeleteall.png)
+
+## 6. User Make Admin
+![Alt text](screenshot/tugas6/Screenshotusermakeadmin.png)
+
+## 7. User Remove Admin
+![Alt text](screenshot/tugas6/Screenshotuserremoveadmin.png)
+
+## 8. User Delete
+![Alt text](screenshot/tugas6/Screnshotuserdelete.png)

92a685ba6def3e270c3433a8456e9dc0a3ac5f30
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-06-15 23:08:43 +0700

    api : logout, create, update, delete data

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Dedoc\Scramble\Support\Generator\Response;
+
 
 class AuthController extends Controller
 {
@@ -46,7 +49,6 @@ class AuthController extends Controller
                     'token' => $token,
                 ]
             ], 200);
-
         } catch (Exception $e) {
             return response()->json([
                 'status_code' => 500,
@@ -58,11 +60,47 @@ class AuthController extends Controller
     /**
      * Logout user yang sedang login.
      */
-    public function logout()
+    // public function logout()
+    // {
+    //     Auth::guard('api')->logout();
+    //     return response()->json([
+    //         'message' => 'Logout berhasil',
+    //     ], 200);
+    // }
+
+    /**
+     * Logout pengguna yang sedang login.
+     *
+     * Menghapus token JWT agar tidak bisa digunakan lagi.
+     */
+    #[Response(
+        status: 200,
+        content: [
+            'status_code' => 200,
+            'message' => 'Logout berhasil. Token telah dihapus.'
+        ]
+    )]
+    #[Response(
+        status: 500,
+        content: [
+            'status_code' => 500,
+            'message' => 'Gagal logout, terjadi kesalahan.'
+        ]
+    )]
+    public function logout(Request $request)
     {
-        Auth::guard('api')->logout();
-        return response()->json([
-            'message' => 'Logout berhasil',
-        ], 200);
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Logout berhasil. Token telah dihapus.',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Gagal logout, terjadi kesalahan.',
+            ], 500);
+        }
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Dedoc\Scramble\Support\Generator\OpenApi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,13 +29,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
-        
-        Gate::define('admin', function ($user) {
+
+        Gate::define('admin',  function ($user): bool {
             return $user->is_admin == true;
         });
 
-        Scramble::configure()->routes(function (Route $route) {
-            return Str::startsWith($route->uri, 'api/');
-        });
+        Scramble::configure()
+            ->routes(routeResolver: function (Route $route): bool {
+                return Str::startsWith(haystack: $route->uri, needles: 'api/');
+            })
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    securityScheme: SecurityScheme::http(scheme: 'bearer')
+                );
+            });
     }
 }
